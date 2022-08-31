b02852f2b22508c4d6d7889892f33713820fa9b9
Fernando Warno <ferwar@hotmail.com>
2022-08-31 05:18:33 -0300

    Creacion de render_functions.js

--- js/button.js
+++ js/button.js
@@ -1,9 +1,20 @@
+// El render2 puede tener un alias (otro nombre), 
+// porque es el que NO se exporta por defecto,
+// y se puede importar con el nombre que queramos:
+import render, { render2 } from "./render_functions.js";
+
 export default class Button {
     constructor(text) {
         this.text = text;
     }
 
-    render() {
+    /* render() {
         return `<button>${this.text}</button>`;
-    }
+    } */
 }
+
+// Puedo hacer modificaciones a la clase Button
+// para cambiar el comportamiento de la función render
+// cambio el método render() por el método render2()
+Button.prototype.render = render;
+// Button.prototype.render = render2;
